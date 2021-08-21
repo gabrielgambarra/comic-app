e11a67f803cf24766ef20cdaf7ab69b35f461979
Gabriel Gambarra <gambarragabriel@gmail.com>
2021-08-21 18:09:30 -0400

    Prepare Home page to recive data from API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getData } from "../../api";
+import CharacterCard from "../../components/Home/CharacterCard";
+import { HomeContainer, CharacterGrid } from "./style";
+import SearchBar from "../../components/Home/SearchBar";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    getAPIData();
+  }, []);
+
+  const getAPIData = async () => {
+    const { results } = await getData("/characters", { limit: 9 });
+    setCharacters(results);
+  };
+
+  return (
+    <HomeContainer>
+      <SearchBar onChange={setSearchTerm} value={searchTerm} />
+      <CharacterGrid>
+        {characters.map((char) => (
+          <CharacterCard
+            key={char.id}
+            character={char}
+            to={`/details/${char.id}`}
+          />
+        ))}
+      </CharacterGrid>
+    </HomeContainer>
+  );
 };
 
 export default Home;
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,8 +1,16 @@
 import styled from "styled-components";
-import { Flex } from "../../shared/style";
+import { Flex } from "../../shared/styles";
 
 export const HomeContainer = styled(Flex)`
   flex-direction: column;
   width: 100%;
   height: 100%;
+`;
+
+export const CharacterGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 0.5em;
+  width: 100%;
+  margin: 1em 0;
 `;
