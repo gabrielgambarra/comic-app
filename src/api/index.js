bd71e082b6d68614e4e186faee39fe1cc342768f
Gabriel Gambarra <gambarragabriel@gmail.com>
2021-08-24 17:14:55 -0400

    Update CORS proxy for last time

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const comicvineURL = "https://comicvine.gamespot.com/api/";
-const corsUrl = "http://localhost:8010/proxy/";
-const baseURL = process.env.REACT_APP_ENV === "dev" ? corsUrl : comicvineURL;
+const corsUrl = "https://evening-badlands-17508.herokuapp.com/";
+const baseURL =
+  process.env.REACT_APP_ENV === "dev" ? corsUrl + comicvineURL : comicvineURL;
 const api_key = process.env.REACT_APP_KEY;
 const format = "json";
 
