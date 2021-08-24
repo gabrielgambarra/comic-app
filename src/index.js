8573ce6a10122a9640f04d9099a8a868a3581f48
Gabriel Gambarra <gambarragabriel@gmail.com>
2021-08-23 23:25:53 -0400

    Wrap App with FavoriteProvider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from "./App";
 import { GlobalStyle } from "./shared/styles";
 import { ThemeProvider } from "styled-components";
 import theme from "./shared/themes";
+import { FavoriteProvider } from "./hooks/useFavorite";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <FavoriteProvider>
+        <App />
+      </FavoriteProvider>
       <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>,
